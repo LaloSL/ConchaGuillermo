d24feec492dd783cbaaf5c282289fef9835d14d6
LaloSL <127783873+LaloSL@users.noreply.github.com>
2023-10-30 14:07:19 -0300

    .

--- contacto.js
+++ contacto.js
@@ -1,3 +1,91 @@
+let form = document.getElementById('myForm');
+let primero = document.querySelector("form input");
+primero.focus();
+let reg_correo = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
+
+function validar() {
+
+  let nombre = document.getElementById('nombre').value;
+  let apellido = document.getElementById('apellido').value;
+  let email = document.getElementById('email').value;
+  let celular = document.getElementById('celular').value;
+
+  let nombreError = document.getElementById('nombreError');
+  let apellidoError = document.getElementById('apellidoError');
+  let emailError = document.getElementById('emailError');
+  let celularError = document.getElementById('celularError');
+  let validado = true;
+  let mensajes = document.getElementById('mensajes');
+
+  // validar nombre 
+  if (nombre.trim() === '') {
+    nombreError.innerHTML = '*El campo NOMBRE debe estar completo*';
+    validado = false;
+  } else if (nombre.trim().length < 3) {
+    nombreError.innerHTML = '*debe contener 3 caracteres como mínimo*';
+    validado = false;
+  } else {
+    nombreError.textContent = '';
+  }
+
+  // validar apellido
+  if (apellido.trim() === '') {
+    apellidoError.innerHTML = '*El campo APELLIDO debe estar completo*';
+    validado = false;
+  } else if (apellido.trim().length < 3) {
+    apellidoError.innerHTML = '*debe contener 3 caracteres como mínimo*';
+    validado = false;
+  } else {
+    apellidoError.textContent = '';
+  }
+
+  // validar celular
+  if (celular.length < 10 || celular.length > 13) {
+    celularError.innerHTML = '*mínimo 10 y máximo 13 números*';
+    validado = false;
+  } else {
+    celularError.textContent = '';
+  }
+
+  // validar correo
+  if (email.trim() === '') {
+    emailError.innerHTML = '*El campo CORREO debe estar completo*';
+    validado = false;
+  } else if (!reg_correo.test(email)) {
+    emailError.innerHTML = '*error debe ser ejemplo: algo@algo.com*';
+    validado = false;
+  } else {
+    emailError.textContent = '';
+  }
+
+  mensajes.innerHTML = '';
+  
+  //mostrar el mensaje abajo, si es falso que hay error sino mostrar los datos
+  if (!validado) {
+    let p = document.createElement("p");
+    p.innerHTML = "Hay errores en el formulario";
+    p.style.textAlign = "center";
+    p.style.color = "red";
+    mensajes.appendChild(p);
+    return false;
+  } else {
+    let p = document.createElement("p");
+    let p2 = document.createElement("p");
+    p2.innerHTML = "Formulario enviado por: ";
+    p2.style.textAlign = "center";
+    mensajes.appendChild(p2);
+    p.innerHTML = "Nombre completo: " + apellido + " " + nombre;
+    mensajes.appendChild(p);
+    form.reset();
+    let p3 = document.createElement("p");
+    p3.innerHTML = "¡Pronto nos comunicaremos contigo!";
+    p3.style.textAlign = "center";
+    mensajes.appendChild(p3);
+    return false; 
+  }
+  
+}
+
 // clase del boton
 const botonInicio = document.querySelector(".button1");
 
